4f62429a8da574c756152a418ff8ca29bf753aa3
Nitin1476 <bedwalnitin857@gmail.com>
2026-09-06 08:44:02 +0530

    added components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import card from './components/card'
+import navbar from './components/navbar'
+
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Nitin Bedwal</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, itaque!</p>
+      </div>
+      {card()}
+      {navbar()}
+    </div>
+  )
+}
+
+export default App
