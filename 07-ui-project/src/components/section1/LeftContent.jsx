86b3bf27f6f731a12db5fe902e655db833d53185
Nitin1476 <bedwalnitin857@gmail.com>
2026-09-13 19:41:14 +0530

    add 04-crrds-project, 05-css and 06-tailwind

---
+++ 07-ui-project/src/components/section1/LeftContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { RiArrowRightUpLine } from "@remixicon/react";
+
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/4 bg-blue-300'>
+        <div>
+            <h3>Prosepective <br/> <span>Customer</span> <br/> Segmentation</h3>
+        </div>
+        <div>
+            <RiArrowRightUpLine size={50}/>
+        </div>
+    </div>
+  )
+}
+
+export default LeftContent
